eee97ed0ead566ac050b7e70da4baeda0aa6d20f
SaiOvO <65205938+SaiOvO@users.noreply.github.com>
2020-05-15 20:14:05 -0700

    finish run

--- src/Stage11.java
+++ src/Stage11.java
@@ -17,14 +17,18 @@ public class Stage11 implements Stage {
 	@Override
 	public void executeRoutine() {
 		System.out.println("You walk around collecting small sticks for kindeling. You find an hold tree that looks like you can chop it down with ease. "
-				+ "As you're collecting wood Archie starts barking behind you. You can't see anything when you are hit in the stomach and see a few leave fly"
+				+ "As you're collecting wood \nArchie starts barking behind you. You can't see anything when you are hit in the stomach and see a few leave fly"
 				+ "around you. Do you (1) Run away, (2) Attempt to fight, or (3) Flirt");
 		
 		option = Game.getGame().nextInt(1, 2);
 		
 		if(option == 1) 
 		{
-			System.out.println("no");
+			System.out.println("You start running in the oposite way of where you think the ghost is. You hear wind following behind you as you and Archie run home."
+					+ " As your running you \nslowly hear the wind noise fading the closer you get to your small house. You relise you have enough supplies for now"
+					+ " and don't want to jepordize your or Archies life anymore and decide to \ntravel into town the next morning to tells others about your "
+					+ "interactions and get a search party together to investigate the woods more. Your party is never found again but every day people see Archie"
+					+ " sitting in front of your house waiting of for your return.");
 		}
 		else if(option == 2)
 		{
